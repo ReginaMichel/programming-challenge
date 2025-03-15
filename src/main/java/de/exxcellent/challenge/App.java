8586f16cb2d3a708a94e3e2dcc390487bc1ec592
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-03-15 20:10:52 +0100

    Added handling of CLI arguments to main method. Added a switch to switch between weather- or football-mode. Added switch to choose the correct file reader for specific file-formats passed to the main method. Added tests for these functionalities to AppTest.java. Added a resources-folder to the test-folder, which contains files for testcases.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,25 +1,62 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.readers.TableFromCSVReader;
+import de.exxcellent.challenge.readers.TableFromFileReader;
+
 /**
- * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
- * design. Read: create your own classes and packages as appropriate.
+ * The entry class for the weather-data programming-challenge. Weather data or football results are read from csv-files
+ * and analysed. Either the day with smallest temperature spread or the football team with smallest goal spread are
+ * calculated.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Regina Michel <regina_michel@outlook.de>
  */
 public final class App {
 
     /**
-     * This is the main entry method of your program.
-     * @param args The CLI arguments passed
+     * Main entry method of reading and analysing weather data or football results stored in csv-files.
+     *
+     * @param args The CLI arguments passed. Possible arguments are
+     *             "--weather" or "--football" as first entry,
+     *             "weather.csv", "football.csv", or other filenames as second entry,
+     *             filepaths as third entry.
+     *             If no arguments are passed, the default is the analysis of weather data.
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        // The default mode of the application is to analyse weather data of the file
+        // src/main/resources/de/exxcellent/challenge/weather.csv, but other modes, data types, or filepaths can be
+        // passed via arguments of the main method:
+        String mode = args.length > 0 ? args[0] : "--weather";
+        String fileName = args.length > 1 ? args[1] : "weather.csv";
+        String filePath = args.length > 2 ? args[2] : "src/main/resources/de/exxcellent/challenge/";
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        // A class to read the specific file format is chosen depending on the extention of the filename:
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".")+1);
+        TableFromFileReader reader;
+        switch (fileExtension) {
+            case "csv":
+                reader = new TableFromCSVReader();
+                break;
+            default:
+                // Since other file formats are not supported yet, an exception is thrown, in case one tries.
+                throw new UnsupportedOperationException("File extension is not supported. Please try .csv-files.");
+        }
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        // Switch to decide if weather or football data should be analysed:
+        switch (mode) {
+            case "--weather":
+                String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+                System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+                break;
+            case "--football":
+                String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+                System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+                break;
+            default:
+                // Other cases are not supported yet. An exception is thrown.
+                throw new UnsupportedOperationException("Analysis mode not supported. Please try \"--weather\" or " +
+                        "\"--football\".");
+        }
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,7 +3,10 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Example JUnit 5 test case.
@@ -18,14 +21,44 @@ class AppTest {
         successLabel = "successful";
     }
 
+    /**
+     * Tests, what happens if one passes file extentions to the main method, which are not supported yet. An
+     * {@link UnsupportedOperationException} with message "File extension is not supported. Please try .csv-files."
+     * should be thrown.
+     */
     @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    void unsupportedFileFormats() {
+        Exception exception = assertThrows(UnsupportedOperationException.class,
+                () -> App.main("--football", "football_xlsxfile.xlsx", "src/test/resources/de/exxcellent/challenge/"));
+        assertEquals("File extension is not supported. Please try .csv-files.", exception.getMessage());
     }
 
+    /**
+     * Tests, what happens if one passes a mode to the main method, which is not supported yet. An
+     * {@link UnsupportedOperationException} with message "Analysis mode not supported. Please try \"--weather\" or " +
+     *                 "\"--football\"." should be thrown.
+     */
+    @Test
+    void unsupportedModes() {
+        Exception exception = assertThrows(UnsupportedOperationException.class,
+                () -> App.main("--elections", "weather.csv", "src/main/resources/de/exxcellent/challenge/"));
+        assertEquals("Analysis mode not supported. Please try \"--weather\" or " +
+                "\"--football\".", exception.getMessage());
+    }
+
+    /**
+     * Tests, if default-mode of the main-method is run.
+     */
+    @Test
+    void runDefault() {
+        App.main();
+    }
+
+    /**
+     * Tests, if football-mode of the main-method is run.
+     */
     @Test
     void runFootball() {
         App.main("--football", "football.csv");
     }
-
 }
